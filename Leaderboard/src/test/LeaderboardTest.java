44dce5008e7e47e64a541389db79f8db39471468
Nori West <yshaggy@live.unc.edu>
2022-11-08 10:03:01 -0500

    skeleton

--- Leaderboard/src/test/LeaderboardTest.java
+++ Leaderboard/src/test/LeaderboardTest.java
@@ -1,5 +1,23 @@
 package test;
 
 
+import org.junit.Test;
+
 public class LeaderboardTest {
+    @Test
+    public void testConstructor() {}
+    @Test
+    public void testAddScore() {}
+    @Test
+    public void testDeleteScore() {}
+    @Test
+    public void testGetScore() {}
+    @Test
+    public void testGetTopPlayer() {}
+    @Test
+    public void testGetTopScore() {}
+    @Test
+    public void testReset() {}
 }
+
+
